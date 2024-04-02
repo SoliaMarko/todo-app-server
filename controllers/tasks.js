95d72de249d50f34ad44422fc3ddbe88ffdf3ad2
solomiia-marko <soliamark25@gmail.com>
2024-04-02 19:03:19 +0300

    fix: fixed status codes and extracted them into constants

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,13 +1,14 @@
-const Tasks = require('../models/Task.model');
+const { success, clientError } = require('../constants/statusCodes.constant');
+const Tasks = require('../models/task.model');
 
 // @desc    Get all tasks
 // @route   GET /api/v1/tasks
 exports.getTasks = async (req, res, next) => {
   try {
     const tasks = await Tasks.find();
-    res.status(200).json({ success: true, data: tasks });
+    res.status(success.OK).json({ success: true, data: tasks });
   } catch ({ message }) {
-    res.status(404).json({ success: false, message });
+    res.status(clientError.NOT_FOUND).json({ success: false, message });
   }
 };
 
@@ -17,11 +18,11 @@ exports.getTask = async (req, res, next) => {
   try {
     const task = await Tasks.findById(req.params.id);
     if (!task) {
-      return res.status(400).json({ success: false });
+      return res.status(clientError.NOT_FOUND).json({ success: false });
     }
-    res.status(200).json({ success: true, data: task });
+    res.status(success.OK).json({ success: true, data: task });
   } catch ({ message }) {
-    res.status(404).json({ success: false, message });
+    res.status(clientError.NOT_FOUND).json({ success: false, message });
   }
 };
 
@@ -30,20 +31,39 @@ exports.getTask = async (req, res, next) => {
 exports.createTask = async (req, res, next) => {
   try {
     const task = await Tasks.create(req.body);
-    res.status(201).json({ success: true, data: task });
+    res.status(success.CREATED).json({ success: true, data: task });
   } catch ({ message }) {
-    res.status(400).json({ success: false, message });
+    res.status(clientError.BAD_REQUEST).json({ success: false, message });
   }
 };
 
 // @desc    Update task
 // @route   PUT /api/v1/tasks/:id
-exports.updateTask = (req, res, next) => {
-  res.status(200).json({ success: true, msg: `Update task ${req.params.id}` });
+exports.updateTask = async (req, res, next) => {
+  try {
+    const task = await Tasks.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(clientError.BAD_REQUEST).json({ success: false });
+    }
+    res.status(success.CREATED).json({ success: true, data: task });
+  } catch ({ message }) {
+    res.status(clientError.BAD_REQUEST).json({ success: false, message });
+  }
 };
 
 // @desc    Delete task
 // @route   DELETE /api/v1/tasks/:id
-exports.deleteTask = (req, res, next) => {
-  res.status(200).json({ success: true, msg: `Delete task ${req.params.id}` });
+exports.deleteTask = async (req, res, next) => {
+  try {
+    const task = await Tasks.findByIdAndDelete(req.params.id);
+    if (!task) {
+      return res.status(clientError.BAD_REQUEST).json({ success: false });
+    }
+    res.status(success.OK).json({ success: true, data: {} });
+  } catch (err) {
+    res.status(clientError.BAD_REQUEST).json({ success: false, message });
+  }
 };
